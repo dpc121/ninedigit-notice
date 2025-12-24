5a1aa6d3bced6d30c5e68c0124f59c8eebd2ce8d
dpc121 <124233464+dpc121@users.noreply.github.com>
2025-12-25 00:25:19 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open("notice-app").then(c=>{
+      return c.addAll([
+        "index.html",
+        "manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
+});
